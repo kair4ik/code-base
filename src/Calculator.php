a99ed1f724d0fb2c5cc5d92dd4c65d0d327a2ac5
kair4ik <kair.21@mail.ru>
2020-11-17 22:21:05 +0600

    first commit

---
+++ src/Calculator.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Calculator;
+
+
+class Calculator
+{
+
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+    public function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+
+}
